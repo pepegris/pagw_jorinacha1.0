7a6cc98a07b4c7a7300359acee2b6f4c9baab7ae
pepegris <44298815+pepegris@users.noreply.github.com>
2020-03-25 18:13:53 -0400

    actualizacion
    se le puso un color lila con blanco se creo el formulario-php falta la prueba de envio de correo

---
+++ contactform/formulario.php
@@ -0,0 +1,32 @@
+<?php
+
+
+    if (isset($_POST['Send Message'])) {
+
+
+      if (!empty($_POST['name'])&& !empty($_POST['subject']) && !empty($_POST['message'])&&!empty($_POST['email'])) {
+
+            $name= $_POST['name'];
+                $asunto= $_POST['subject'];
+                    $mensaje= $_POST['message'];
+                        $correo= $_POST['email'];
+        
+
+
+
+
+      }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
